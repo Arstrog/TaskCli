eb9566de7bb1a66ddcbbbebd0ba1d09b65a38b68
Arstrog <thergos8794@gmail.com>
2026-09-25 21:33:07 +0300

    Finished with parser, created add,update,delete,mark and list methods

--- tasker.py
+++ tasker.py
@@ -2,6 +2,8 @@
 # store tasks in json file
 # User:
 #--add
+# --delete
+# update
 #--mark task (in progress) or done
 #--list all tasks
 #--list all completed tasks
@@ -9,72 +11,125 @@
 #--list all not started
 from datetime import datetime
 import json
-from os import path
+import os
 import sys
 import argparse
 
 
 
-
-
-def add(database, description) -> None:
+def add(args,database) -> None:
 
     id = str(max(map(int,database.keys()),default=0)+1)
-    today=datetime.now().strftime("%d %m %Y %H %M %S")
+    today=datetime.now().strftime("%d-%m-%Y:%H:%M:%S")
     database[id] = {
-        "description": description,
+        "description": args.description,
         "status":"todo",
         "createdAt":today,
         "updatedAt":today
     }
+    #for now:
+    print(f"Task added successfully (Id: {id})")
 
-    return list_task(id,database[id])
+def update(args,database,status=None):
+    #args needed are id and description
+    id = str(args.id)
+    description = str(args.description)
+    status = str(args.status)
+    if status is not None:
+        database[id]["status"]=status
+    if id in database:
+        database[id]["description"]=description
+        database[id]["updatedAt"]=datetime.now().strftime("%d-%m-%Y:%H:%M:%S")
 
-def list_task(id,data):
-    #prepei na einai se morphy pinaka, pio meta omws
-    print(f"id:{id} | {data}")
 
-def mark():
-    pass
+def delete(args,database):
+    id = str(args.id)
+    if int(id)<=0 or (id not in database):
+        sys.exit(f"Task with id:{id} does not exist.")
+    database.pop(id)
 
-def list_all_tasks():
-    pass
+def mark_in_progress(args,database):
+    update(args,database,status="in-progress")
 
-def list_all_completed_tasks():
-    pass
+def mark_done(args,database):
+    update(args,database,status="done")
 
-def list_in_progress():
-    pass
+def list_task(args,database):
 
-def list_not_completed():
-    pass
+    print ("{:<8} {:<30} {:<10} {:<20} {:<15}".format('Id','description','status','createdAt','updatedAt'))
+    for id ,data in database.items():
+        vals=data.values()
+        des,stat,create,update = vals
+        print(f"{id:<8} {des:<30} {stat:<10} {create:<20} {update:<15}")
+
+
 
 #for the json file
 def save(database_path,data):
-    with open("database_path","w") as f:
+    with open(database_path,"w") as f:
         json.dump(data,f)
 
 def load(database_path):
     try:
-        with open("database_path","r") as f:
+        with open(database_path,"r") as f:
             tasks= json.load(f)
             return tasks
-    except FileNotFoundError:
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
         return {}
 
 
-# def _arg_parse_conf() -> argparse.ArgumentParser :
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("-db", default="~/tasker.json",help="Database path.")
-#     parser.add_argument("add",help="Adds an item to the database")
-#
+def _arg_parse_conf() -> argparse.ArgumentParser :
+    parser = argparse.ArgumentParser(prog="tasker")
+    subparsers = parser.add_subparsers(dest="commands",required=True)
+    #add method
+    parse_add = subparsers.add_parser("add",help="Add a new task.")
+    parse_add.add_argument("description",type=str)
+    parse_add.set_defaults(func=add)
+
+    #update
+    parse_update = subparsers.add_parser("update",help="Update the description of a task.")
+    parse_update.add_argument("id",type=int)
+    parse_update.add_argument("description",type=str)
+    # parse_update.add_argument("status",nargs="?",choices=["todo","in-progress","done"],default=None)
+    parse_update.set_defaults(func=update)
+
+    #delete
+    parse_delete = subparsers.add_parser("delete",help="Delete a task with the given id.")
+    parse_delete.add_argument("id",type=int)
+    parse_delete.set_defaults(func=delete)
+
+    #mark
+    parent_mark = argparse.ArgumentParser(add_help=False)
+    parent_mark.add_argument("id",type=int,help="Task id.")
+    parse_mark_done = subparsers.add_parser("mark-done",help="Mark the status of the current task as done.",parents=[parent_mark])
+    parse_mark_done.set_defaults(func=mark_done)
+    parse_mark_progress = subparsers.add_parser("mark-in-progress",help="Mark the status of the current task as in-progress.",parents=[parent_mark])
+    parse_mark_progress.set_defaults(func=mark_in_progress)
+
+    #list
+    parse_list=subparsers.add_parser("list",help="List all tasks.")
+    parse_list.add_argument("status",nargs="?",choices=["todo","in-progress","done"],default=None)
+    parse_list.set_defaults(func=list_task)
+
+    return parser
+
+
+
+
+
+
 
 
 def main():
+    db = load("tests.json")
+    parser = _arg_parse_conf()
+    args=parser.parse_args()
+    args.func(args,db)
+    save("tests.json",db)
 
-    pass
+
 
 
 
 if __name__=="__main__":
-    main()
+    sys.exit(main())
